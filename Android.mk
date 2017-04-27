a76767b9aa5d56ee076a67061d4a675dc0a8c5e4
Vachounet <vachounet@live.fr>
2017-04-27 16:08:23 +0200

    potter: add com.motorola.camerabgproc

--- Android.mk
+++ Android.mk
@@ -145,6 +145,16 @@ LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := com.motorola.camerabgproc.jar
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/framework/com.motorola.camerabgproc.jar
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := .jar
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := com.qti.snapdragon.sdk.display
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/framework/com.qti.snapdragon.sdk.display.jar
