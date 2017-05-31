fc4fc59afcb57aad59a90f89d930ed289989fb36
wzedlare <vedatak01@gmail.com>
2017-05-31 10:34:38 +0000

    cedric: Move libril to fix warning

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ ifeq ($(TARGET_DEVICE),cedric)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libril
 LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/lib/libril.so
+LOCAL_SRC_FILES := lib/libril.so
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
