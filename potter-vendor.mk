9e19582d4de1cdfb440f4e95376577497c1900f7
Vachounet <vachounet@live.fr>
2017-08-20 16:34:15 +0200

    potter: ship libuiblur

--- potter-vendor.mk
+++ potter-vendor.mk
@@ -893,6 +893,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/potter/proprietary/vendor/lib/libsd_sdk_display.so:system/vendor/lib/libsd_sdk_display.so \
     vendor/motorola/potter/proprietary/vendor/lib/egl/libq3dtools_esx.so:system/vendor/lib/egl/libq3dtools_esx.so \
     vendor/motorola/potter/proprietary/lib/libjson.so:system/lib/libjson.so \
+    vendor/motorola/potter/proprietary/lib/libuiblur.so:system/lib/libuiblur.so \
     vendor/motorola/potter/proprietary/vendor/lib/libsdm-color.so:system/vendor/lib/libsdm-color.so \
     vendor/motorola/potter/proprietary/vendor/lib/libsdm-diag.so:system/vendor/lib/libsdm-diag.so \
     vendor/motorola/potter/proprietary/vendor/lib/libsdm-disp-apis.so:system/vendor/lib/libsdm-disp-apis.so \
