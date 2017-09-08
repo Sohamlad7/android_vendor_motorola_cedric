12a15ed2b0350be37f4142e2db83992e947ee675
Vachounet <vachounet@live.fr>
2017-09-08 13:40:56 +0200

    potter: rename sensors HAL for multiHAL

--- potter-vendor.mk
+++ potter-vendor.mk
@@ -141,7 +141,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/potter/proprietary/lib/hw/camera.vendor.msm8953.so:system/lib/hw/camera.vendor.msm8953.so \
     vendor/motorola/potter/proprietary/lib/hw/fingerprint.msm8953.so:system/lib/hw/fingerprint.msm8953.so \
     vendor/motorola/potter/proprietary/lib/hw/sensorhub.msm8953.so:system/lib/hw/sensorhub.msm8953.so \
-    vendor/motorola/potter/proprietary/lib/hw/sensors.msm8953.so:system/lib/hw/sensors.msm8953.so \
+    vendor/motorola/potter/proprietary/lib/hw/sensors.msm8953.so:system/lib/sensors.moto.so \
     vendor/motorola/potter/proprietary/lib/lib_fpc_tac_shared.so:system/lib/lib_fpc_tac_shared.so \
     vendor/motorola/potter/proprietary/lib/lib_motsensorlistener.so:system/lib/lib_motsensorlistener.so \
     vendor/motorola/potter/proprietary/lib/lib_mottof.so:system/lib/lib_mottof.so \
@@ -904,7 +904,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \
     embms \
-    CNEService \
     colorservice \
     com.qti.location.sdk \
     com.qti.snapdragon.sdk.display \
@@ -913,8 +912,6 @@ PRODUCT_PACKAGES += \
     imscmlibrary \
     qcrilhook \
     com.qualcomm.location \
-    ims \
-    imssettings \
     qcrilmsgtunnel \
     izat.xt.srv \
     qti-telephony-common \
