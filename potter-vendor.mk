9ed06404d1a29b434e7886f77d7d536fb8d29aa5
Vachounet <vachounet@live.fr>
2017-09-11 14:24:32 +0200

    Revert "potter: rename sensors HAL for multiHAL"
    (reverted from commit 12a15ed2b0350be37f4142e2db83992e947ee675)

--- potter-vendor.mk
+++ potter-vendor.mk
@@ -135,7 +135,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/potter/proprietary/lib/hw/camera.vendor.msm8953.so:system/lib/hw/camera.vendor.msm8953.so \
     vendor/motorola/potter/proprietary/lib/hw/fingerprint.msm8953.so:system/vendor/lib/hw/fingerprint.vendor.msm8953.so \
     vendor/motorola/potter/proprietary/lib/hw/sensorhub.msm8953.so:system/lib/hw/sensorhub.msm8953.so \
-    vendor/motorola/potter/proprietary/lib/hw/sensors.msm8953.so:system/lib/sensors.moto.so \
+    vendor/motorola/potter/proprietary/lib/hw/sensors.msm8953.so:system/lib/hw/sensors.msm8953.so \
     vendor/motorola/potter/proprietary/lib/lib_fpc_tac_shared.so:system/lib/lib_fpc_tac_shared.so \
     vendor/motorola/potter/proprietary/lib/lib_motsensorlistener.so:system/lib/lib_motsensorlistener.so \
     vendor/motorola/potter/proprietary/lib/lib_mottof.so:system/lib/lib_mottof.so \
@@ -898,6 +898,7 @@ PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \
     embms \
+    CNEService \
     colorservice \
     com.qti.location.sdk \
     com.qti.snapdragon.sdk.display \
@@ -906,6 +907,8 @@ PRODUCT_PACKAGES += \
     imscmlibrary \
     qcrilhook \
     com.qualcomm.location \
+    ims \
+    imssettings \
     qcrilmsgtunnel \
     izat.xt.srv \
     qti-telephony-common \
