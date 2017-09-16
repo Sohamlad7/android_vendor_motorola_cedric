66570228c6a4f36f3d2267894aa4d30616e40b3e
Vachounet <vachounet@live.fr>
2017-09-16 13:28:29 +0200

    potter: do not ship CNEService

--- potter-vendor.mk
+++ potter-vendor.mk
@@ -908,7 +908,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \
     embms \
-    CNEService \
     colorservice \
     com.qti.location.sdk \
     com.qti.snapdragon.sdk.display \
