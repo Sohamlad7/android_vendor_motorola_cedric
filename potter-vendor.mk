0e4e96488b6c5152964b4f89874228ba1c048b13
Vachounet <vachounet@live.fr>
2017-09-16 13:26:28 +0200

    potter: ship N libgui and libui
    * Renamed and haxxed to libguy and libuy

--- potter-vendor.mk
+++ potter-vendor.mk
@@ -900,7 +900,10 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/potter/proprietary/lib/libqsap_sdk.so:system/lib/libqsap_sdk.so \
     vendor/motorola/potter/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
     vendor/motorola/potter/proprietary/vendor/lib/libpanelmode.so:system/vendor/lib/libpanelmode.so \
-    vendor/motorola/potter/proprietary/vendor/lib/libbson.so:system/vendor/lib/libbson.so
+    vendor/motorola/potter/proprietary/vendor/lib/libbson.so:system/vendor/lib/libbson.so \
+    vendor/motorola/potter/proprietary/vendor/lib/libguy.so:system/vendor/lib/libguy.so \
+    vendor/motorola/potter/proprietary/vendor/lib/libuy.so:system/vendor/lib/libuy.so \
+
 PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \
