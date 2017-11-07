ebafc2025b4d3b8016d01a541b8ea288dced2a1d
Vachounet <vachounet@live.fr>
2017-11-07 14:52:11 +0100

    Revert "potter: do not ship CNEService"
    (reverted from commit 66570228c6a4f36f3d2267894aa4d30616e40b3e) (reverted from commit 343bba5c0a71cbc94ff8127b894c9b721d91d45c)

--- potter-vendor.mk
+++ potter-vendor.mk
@@ -911,7 +911,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     datastatusnotification \
     embms \
-    CNEService \
     colorservice \
     com.qti.location.sdk \
     com.qti.snapdragon.sdk.display \
