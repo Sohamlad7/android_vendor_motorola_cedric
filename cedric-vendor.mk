629335ec527cedc080e02a56676fcb1cfbcc343d
Vachounet <vachounet@live.fr>
2017-11-25 09:24:18 +0100

    cedric: ship prebuilt libgnss

--- cedric-vendor.mk
+++ cedric-vendor.mk
@@ -1024,6 +1024,8 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/cedric/proprietary/vendor/lib64/libflp.so:system/vendor/lib64/libflp.so \
     vendor/motorola/cedric/proprietary/vendor/lib64/libgdtap.so:system/vendor/lib64/libgdtap.so \
     vendor/motorola/cedric/proprietary/vendor/lib64/libgnsspps.so:system/vendor/lib64/libgnsspps.so \
+    vendor/motorola/cedric/proprietary/vendor/lib64/libgnss.so:system/vendor/lib64/libgnss.so \
+    vendor/motorola/cedric/proprietary/vendor/lib/libgnss.so:system/vendor/lib/libgnss.so \
     vendor/motorola/cedric/proprietary/vendor/lib64/liblowi_client.so:system/vendor/lib64/liblowi_client.so \
     vendor/motorola/cedric/proprietary/vendor/lib64/liblowi_wifihal.so:system/vendor/lib64/liblowi_wifihal.so \
     vendor/motorola/cedric/proprietary/vendor/lib64/libxtadapter.so:system/vendor/lib64/libxtadapter.so \
