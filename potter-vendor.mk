586611c77f67d6312fb541981a377a7a66d1a308
Bruno Martins <bgcngm@gmail.com>
2018-03-15 00:44:38 +0000

    potter: Use O-MR1 based GPS blobs
     * From: bq/bardock-pro/bardock-pro:8.1.0/OPM1.171019.011/1651:user/release-keys
    
    Change-Id: Ia5bf9dcdd417a301cb4952c44b63fb063917502b

--- potter-vendor.mk
+++ potter-vendor.mk
@@ -1178,7 +1178,12 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/potter/proprietary/lib/vendor.qti.hardware.qdutils_disp@1.0.so:system/lib/vendor.qti.hardware.qdutils_disp@1.0.so \
     vendor/motorola/potter/proprietary/vendor/etc/init/vendor.qti.hardware.qdutils_disp@1.0-service-qti.rc:system/vendor/etc/init/vendor.qti.hardware.qdutils_disp@1.0-service-qti.rc \
     vendor/motorola/potter/proprietary/vendor/bin/hw/vendor.qti.hardware.qdutils_disp@1.0-service-qti:system/vendor/bin/hw/vendor.qti.hardware.qdutils_disp@1.0-service-qti \
-    vendor/motorola/potter/proprietary/vendor/lib64/vendor.qti.hardware.qdutils_disp@1.0_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.qdutils_disp@1.0_vendor.so
+    vendor/motorola/potter/proprietary/vendor/lib64/vendor.qti.hardware.qdutils_disp@1.0_vendor.so:system/vendor/lib64/vendor.qti.hardware.qdutils_disp@1.0_vendor.so \
+    vendor/motorola/potter/proprietary/lib64/liblocationservice_jni.so:system/lib64/liblocationservice_jni.so \
+    vendor/motorola/potter/proprietary/vendor/lib64/libquipc_os_api.so:system/vendor/lib64/libquipc_os_api.so \
+    vendor/motorola/potter/proprietary/lib64/libxt_native.so:system/lib64/libxt_native.so \
+    vendor/motorola/potter/proprietary/vendor/lib64/libxtwifi_ulp_adaptor.so:system/vendor/lib64/libxtwifi_ulp_adaptor.so \
+    vendor/motorola/potter/proprietary/vendor/bin/lowi-server:system/vendor/bin/lowi-server
 
 PRODUCT_PACKAGES += \
     TimeService \
