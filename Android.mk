40497f4f3c1c8e874eea62a8ead27e4b555df0a4
Vachounet <vachounet@live.fr>
2019-01-23 17:24:32 +0100

    cedric: update blobs for libperfmgr
    Change-Id: I35ad633f340765fec5305bb9c6c608385184256a

--- Android.mk
+++ Android.mk
@@ -341,4 +341,12 @@ LOCAL_MODULE_SUFFIX := .apk
 LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
-endif
+include $(CLEAR_VARS)
+LOCAL_MODULE := UxPerformance
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_SRC_FILES := proprietary/framework/UxPerformance.jar
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := .jar
+include $(BUILD_PREBUILT)
